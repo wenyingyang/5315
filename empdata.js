34275ffd70b2fbb2ed9d4392fed79558e1329ea9
wenyingyang <yangw3@seattleu.edu>
2018-05-27 00:25:19 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -1,152 +1,152 @@
 {
   "data": [
     {
-      "Order Date": "1/6/2016",
+      "OrderDate": "1/6/2016",
       "Region": " East ",
       "Rep": "Jones",
       "Item": " Pencil",
       "Units": "95",
-      "Unit Cost": "1.99"
+      "UnitCost": "1.99"
       "Total": "189.05"
 
     },
     {
-      "Order Date": "1/23/2016",
+      "OrderDate": "1/23/2016",
       "Region": "Central",
       "Rep": "Kivell",
       "Item": "Binder",
       "Units": "50",
-      "Unit Cost": "19.99"
+      "UnitCost": "19.99"
       "Total": "999.50"
 
     },
     {
-      "Order Date": "2/9/2016",
+      "OrderDate": "2/9/2016",
       "Region ": "Central",
       "Rep": "Jardine",
-      "Item ": "Pencil",
+      "Item": "Pencil",
       "Units": "36",
-      "Unit Cost": "4.99"
+      "UnitCost": "4.99"
       "Total": "179.64"
 
     },
     {
-      "Order Date": "2/26/2016",
+      "OrderDate": "2/26/2016",
       "Region": "Central",
       "Rep": "Gill",
       "Item": "Pen",
       "Units": "27",
-      "Unit Cost": "19.99"
+      "UnitCost": "19.99"
       "Total": "539.73"
 
     },
     {
-      "Order Date": "3/15/2016",
+      "OrderDate": "3/15/2016",
       "Region": "West",
       "Rep": "Sorvino",
       "Item": "Pencil",
       "Units": "56",
-      "Unit Cost": "2.99"
+      "UnitCost": "2.99"
       "Total": "167.44"
 
     },
     {
-      "Order Date": "4/1/2016",
+      "OrderDate": "4/1/2016",
       "Region": "East",
       "Rep": "Jones",
       "Item": "Binder",
       "Units": "60",
-      "Unit Cost": "4.99"
+      "UnitCost": "4.99"
       "Total": "299.40"
 
     },
     {
-      "Order Date": "5/5/2016",
+      "OrderDate": "5/5/2016",
       "Region": "Central",
       "Rep": "Jardine",
       "Item": "Pencil",
       "Units": "90",
-      "Unit Cost": "4.99"
+      "UnitCost": "4.99"
       "Total": "449.10"
 
     },
     {
-      "Order Date": "4/18/2016",
+      "OrderDate": "4/18/2016",
       "Region": "Central",
       "Rep": "Andrews",
       "Item": "Pencil",
       "Units": "75",
-      "Unit Cost": "1.99"
+      "UnitCost": "1.99"
       "Total": "149.25"
 
     },
     {
-      "Order Date": "5/22/2016",
+      "OrderDate": "5/22/2016",
       "Region": "West",
       "Rep": "Thompson",
       "Item": "Pencil",
       "Units": "32",
-      "Unit Cost": "1.99"
+      "UnitCost": "1.99"
       "Total": "63.68"
 
     },
     {
-      "Order Date": "6/8/2016",
+      "OrderDate": "6/8/2016",
       "Region": "East",
       "Rep": "Jones",
       "Item": "Binder",
       "Units": "60",
-      "Unit Cost": "8.99"
+      "UnitCost": "8.99"
       "Total": "539.40"
 
     },
     {
-      "Order Date": "6/25/2016",
+      "OrderDate": "6/25/2016",
       "Region": "Central",
       "Rep": "Morgan",
       "Item": "Pencil",
       "Units": "90",
-      "Unit Cost": "4.99"
+      "UnitCost": "4.99"
       "Total": "449.10"
 
     },
     {
-      "Order Date": "7/12/2016",
+      "OrderDate": "7/12/2016",
       "Region": "East",
       "Rep": "Howard",
       "Item": "Binder",
       "Units": "29",
-      "Unit Cost": "1.99"
+      "UnitCost": "1.99"
       "Total": "57.71"
 
     },
     {
-      "Order Date": "7/29/2016",
+      "OrderDate": "7/29/2016",
       "Region": "East",
       "Rep": "Parent",
       "Item": "Binder",
       "Units": "81",
-      "Unit Cost": "19.99"
+      "UnitCost": "19.99"
       "Total": "1,619.19"
 
     },
     {
-      "Order Date": "8/15/2016",
+      "OrderDate": "8/15/2016",
       "Region": "East",
       "Rep": " Jones ",
       "Item": " Pencil ",
       "Units": "35",
-      "Unit Cost": "4.99"
+      "UnitCost": "4.99"
       "Total": "174.65"
 
     },
     {
-      "Order Date": "9/1/2016",
+      "OrderDate": "9/1/2016",
       "Region": "Central",
       "Rep": "Smith",
       "Item": "Desk",
       "Units": "2",
-      "Unit Cost": "125.00"
+      "UnitCost": "125.00"
       "Total": "250.00"
 
     }
